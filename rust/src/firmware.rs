5f5849952e91407645e564421c004ad529cfb1bb
hut <hut@hut.pm>
2024-05-09 05:17:00 +0200

    copy over another UUID constant from arduino code to rust

--- rust/src/firmware.rs
+++ rust/src/firmware.rs
@@ -2,3 +2,4 @@
 // not the firmware itself.
 
 pub const SENSOR_CHARACTERISTICS_UUID: &str = "0a3d3fd8-2f1c-46fd-bf46-eaef2fda91e5";
+pub const CHANNEL_COUNT_CHARACTERISTICS_UUID: &str = "0a3d3fd8-2f1c-46fd-bf46-eaef2fda91e6";
