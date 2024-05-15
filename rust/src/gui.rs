5e3f6c557d8e76a7db608e7014f75aa9b7ec30db
hut <hut@hut.pm>
2024-05-16 00:26:29 +0200

    pass device out of the loop

--- rust/src/gui.rs
+++ rust/src/gui.rs
@@ -7,14 +7,17 @@ pub fn start(app: base::App) {
 
     app.rt.block_on(async {
         tokio::spawn(async move {
-            loop {
+            let device = loop {
                 tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
                 if let Ok(device) = bluetooth::find_peripheral().await {
-                    ui_weak.upgrade_in_event_loop(move |ui| {
-                        ui.set_mytext(format!("Found PsyLink with MAC address {}.\n\nConnecting...", device.address).into());
-                    }).unwrap();
+                    let address = device.address.clone();
+                    let _ = ui_weak.upgrade_in_event_loop(move |ui| {
+                        ui.set_mytext(format!("Found PsyLink with MAC address {address}.\n\nConnecting...").into());
+                    });
+                    break device;
                 }
-            }
+            };
+
         });
     });
     ui.run().unwrap();
