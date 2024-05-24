a5b4938c0e990ea8f46873f91834c942a653c016
yuni <hut@hut.pm>
2024-05-24 23:22:49 +0200

    add plotters lib, draw a graph of the first signal

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,6 @@
 use crate::{base, bluetooth, protocol};
+use plotters::prelude::*;
+use slint::SharedPixelBuffer;
 slint::include_modules!();
 
 pub async fn start(app: base::App) {
@@ -30,7 +32,11 @@ pub async fn start(app: base::App) {
 
         loop {
             let bytearray: Vec<u8> = device.read().await.unwrap(); // TODO: catch panic
-            let text = bytearray.iter().map(|n| n.to_string()).collect::<Vec<String>>().join(", ");
+            let text = bytearray
+                .iter()
+                .map(|n| n.to_string())
+                .collect::<Vec<String>>()
+                .join(", ");
             println!("Received BLE payload: {};", text);
 
             let packet = decoder.decode_packet(bytearray);
@@ -42,17 +48,47 @@ pub async fn start(app: base::App) {
             let packet = packet.unwrap();
 
             let mut string = String::new();
-            for data in packet.samples {
+            for data in &packet.samples {
                 string += format!("{data:?}\n").as_str();
             }
+            let signal_data = packet.samples[0].clone();
             let _ = ui_weak.upgrade_in_event_loop(move |ui| {
                 ui.set_datatext(string.into());
-                ui.set_graph0(
-                    slint::Image::load_from_svg_data(include_str!("data/icon.svg").as_bytes()).unwrap()
-                );
+                ui.set_graph0(render_plot(signal_data));
             });
             tokio::time::sleep(tokio::time::Duration::from_secs_f32(0.1)).await;
         }
     });
     ui.run().unwrap();
+}
+
+fn render_plot(signal_data: Vec<u8>) -> slint::Image {
+    let mut pixel_buffer = SharedPixelBuffer::new(800, 300);
+    let size = (pixel_buffer.width(), pixel_buffer.height());
+    let backend = BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
+    let root = backend.into_drawing_area();
+    root.fill(&WHITE).expect("error filling drawing area");
+
+    let mut chart = ChartBuilder::on(&root)
+        .build_cartesian_2d(0.0..30.0, 0.0..255.0)
+        .expect("error building coordinate system");
+
+    chart.configure_mesh().draw().expect("error drawing");
+
+    chart
+        .draw_series(LineSeries::new(
+            signal_data
+                .iter()
+                .enumerate()
+                .map(|(i, x)| (i as f64, *x as f64)),
+            &RED,
+        ))
+        .expect("error drawing series");
+
+    root.present().expect("error presenting");
+    drop(chart);
+    drop(root);
+
+    slint::Image::from_rgb8(pixel_buffer)
+    //slint::Image::load_from_svg_data(include_str!("data/icon.svg").as_bytes()).unwrap()
 }
