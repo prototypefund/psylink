fe0687bd1f9318c7fa5c0798b2549069e14f3600
yuni <hut@hut.pm>
2024-08-02 00:14:16 +0200

    add android entry point with "hello world" text

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,17 @@ pub mod prelude {
         pub scantime: f32,
     }
 }
+
+#[no_mangle]
+#[cfg(target_os = "android")]
+fn android_main(app: slint::android::AndroidApp) {
+    slint::android::init(app).unwrap();
+
+    // ... rest of your code ...
+    slint::slint!{
+        export component MainWindow inherits Window {
+            Text { text: "Hello World"; }
+        }
+    }
+    MainWindow::new().unwrap().run().unwrap();
+}
