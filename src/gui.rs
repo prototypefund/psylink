c89729df63e668bcacde325d05c91543ed47bcfb
yuni <hut@hut.pm>
2024-08-15 04:12:08 +0200

    generate calibrator messages, move on to the next action

--- src/gui.rs
+++ src/gui.rs
@@ -121,7 +121,8 @@ pub async fn start(app: App) {
 
 #[derive(Clone, Default)]
 pub struct Calibrator {
-    pub action_count: u32,
+    pub action_count: usize,
+    pub current_action: usize,
     pub remaining_key_presses: Vec<u32>,
     pub timer: f64,
     pub state: CalibratorState,
@@ -140,6 +141,15 @@ pub enum CalibratorState {
 }
 
 impl Calibrator {
+    pub fn new(action_count: usize, key_presses: u32) -> Self {
+        let mut result = Self::default();
+        result.action_count = action_count;
+        for _ in 0..action_count {
+            result.remaining_key_presses.push(key_presses);
+        }
+        result
+    }
+
     /// returns true if a state change happened
     pub fn tick(&mut self, time: f64) -> bool {
         if self.timer > 0.0 {
@@ -158,7 +168,12 @@ impl Calibrator {
                     }
                 }
                 CalibratorState::GestureActionWait => CalibratorState::GestureAction,
-                CalibratorState::GestureAction => CalibratorState::NullActionWait,
+                CalibratorState::GestureAction => {
+                    self.remaining_key_presses[self.current_action] = self.remaining_key_presses[self.current_action].saturating_sub(1);
+                    self.current_action = (self.current_action + 1) % self.action_count;
+
+                    CalibratorState::NullActionWait
+                }
                 CalibratorState::Done => CalibratorState::Done,
             };
             let delay = match new_state {
@@ -174,9 +189,23 @@ impl Calibrator {
             self.state = new_state;
             self.timer = delay;
             return state_change_happened;
-        }
-        else {
+        } else {
             return false;
+        }
+    }
+
+    pub fn generate_message(&self) -> String {
+        let current_action = 1;
+        match self.state {
+            CalibratorState::Init => "Initializing...".into(),
+            CalibratorState::Welcome => {
+                "Calibration starting. Please follow the instructions.".into()
+            }
+            CalibratorState::NullActionWait => "Prepare to rest your arm.".into(),
+            CalibratorState::NullAction => "Rest your arm now.".into(),
+            CalibratorState::GestureActionWait => format!("Prepare movement #{current_action}"),
+            CalibratorState::GestureAction => format!("Do movement #{current_action} now."),
+            CalibratorState::Done => "Calibration complete.".into(),
         }
     }
 }
