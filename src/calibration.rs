be570d55f0983883a334ed58a917ebaba5c1cd58
yuni <hut@hut.pm>
2024-08-16 01:35:46 +0200

    implement burn Dataset for PsyLink Samples

--- src/calibration.rs
+++ src/calibration.rs
@@ -1,10 +1,23 @@
+use burn::data::dataloader::Dataset;
+
+#[derive(Clone, Debug)]
 pub struct Sample {
     pub features: Vec<f64>,
     pub label: u8,
 }
 
+#[derive(Clone, Debug)]
 pub struct Samples {
     pub samples: Vec<Sample>,
+}
+
+impl Dataset<Sample> for Samples {
+    fn get(&self, index: usize) -> Option<Sample> {
+        return self.samples.get(index).cloned();
+    }
+    fn len(&self) -> usize {
+        return self.samples.len();
+    }
 }
 
 #[derive(Clone, Default)]
