e67efe7eedb108adcac15ab95bb4d616f0dc091c
yuni <hut@hut.pm>
2024-08-23 03:21:36 +0200

    explicitly set slint style (to default "native")

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    slint_build::compile("src/psylink.slint").unwrap();
+    let config =
+        slint_build::CompilerConfiguration::new()
+        .with_style("native".into());
+        // .with_style("material".into());
+    slint_build::compile_with_config("src/psylink.slint", config).unwrap();
 }
