5adce802b46e0ba7dbe2444b6df09e0e8e39f636
yuni <hut@hut.pm>
2024-08-23 03:32:10 +0200

    switch to "fluent-dark" slint widget style

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let config =
         slint_build::CompilerConfiguration::new()
-        .with_style("native".into());
-        // .with_style("material".into());
+        // .with_style("native".into());
+        .with_style("fluent-dark".into());
     slint_build::compile_with_config("src/psylink.slint", config).unwrap();
 }
